059f937596b7a4ace15ee93836c4dee5495f76ee
zxkjack123 <zxkjack123@163.com>
2021-01-29 15:07:03 +0800

    part of step2 and decay times

--- tests/files_test_fispact/test.py
+++ tests/files_test_fispact/test.py
@@ -1,9 +1,8 @@
 import pypact as pp
 
-filename = "ve0.out"
+filename = "fispii.out"
 
 with pp.Reader(filename) as output:
-#    import pdb; pdb.set_trace()
     for t in output.inventory_data:
         print(t.irradiation_time)
         print(t.currenttime)
